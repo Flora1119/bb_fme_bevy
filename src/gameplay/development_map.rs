ee9c57871423cb6e7600af55ceb4dd317d0e095b
Flora1119 <ycjun00@naver.com>
2026-09-05 13:49:12 +0900

    feat: normal items test is complete

--- src/gameplay/development_map.rs
+++ src/gameplay/development_map.rs
@@ -5,7 +5,7 @@ use bevy::prelude::*;
 const BLOCK_CONFIG: &str = include_str!("../../assets/config/block_assets_config.json");
 
 const DEVELOPMENT_MAP: &str =
-    include_str!("../../assets/maps/phase5b_ability_teleport_sandbox.json");
+    include_str!("../../assets/maps/phase5b_ability_regression_sandbox.json");
 
 pub struct DevelopmentMapPlugin;
 
